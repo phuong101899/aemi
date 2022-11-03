c489d7a2068176c437b8eb8f6bc4884c6d4ee993
PhuongTa <phuongta@>
2022-11-03 21:08:20 +0700

    feat(Selector): Filter by keyword <phuong.ta>

--- ui/inputs/Input.tsx
+++ ui/inputs/Input.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {InputHTMLAttributes} from "react";
 
 
-interface Props extends React.HTMLProps<HTMLInputElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
     trailing?: JSX.Element,
 }
 
--- ui/selector/Selector.tsx
+++ ui/selector/Selector.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {Input} from "../inputs";
 import Image from "next/image";
-
+import _ from "lodash";
 
 type dataSourceItem = {
     name: string,
@@ -17,6 +17,14 @@ type Props = {
 };
 
 export const Selector: React.FC<Props> = ({title, dataSource, placeholder, maxHeight = 240, onChange}) => {
+    const [keyword, setKeyword] = useState('');
+
+    const getList = (keyword: string) => {
+        return _.filter(dataSource, item => {
+            return _.includes(item.name, keyword);
+        });
+    }
+
     return (
         <div>
             <p className="px-3 text-[22px] font-medium text-secondary font-[700]">
@@ -27,9 +35,11 @@ export const Selector: React.FC<Props> = ({title, dataSource, placeholder, maxHe
                     <Image src={require('ui/icons/assets/search.svg')} alt="search icon" />
                 }
                 placeholder={placeholder}
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
             />
             <div className="mt-4" style={{maxHeight, overflowY: "auto"}}>
-                {dataSource.map((item) => (
+                {getList(keyword).map((item) => (
                     <span
                         key={item.name}
                         onClick={() => onChange(item.code)}
